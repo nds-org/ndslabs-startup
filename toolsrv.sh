e19f85ccc058ed584262685ceefbfd65a42f9a73
CoreOS Admin <core@ndsc5-demo.os.ncsa.edu>
2016-03-31 21:58:46 +0000

    Added pull/tag

--- toolsrv.sh
+++ toolsrv.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-docker run -d -p 8082:8082 --name toolserver -e TOOLSERVER_PORT=8082 -v /var/run/docker.sock:/var/run/docker.sock ndslabs/toolserver:terra toolserver
+docker pull ndslabs/toolserver:0.9.2 
+docker tag ndslabs/toolserver:0.9.2 ncsa/clowder-toolserver
+docker run -d -p 8082:8082 --name toolserver -e TOOLSERVER_PORT=8082 -v /var/run/docker.sock:/var/run/docker.sock ndslabs/toolserver:0.9.2 toolserver
