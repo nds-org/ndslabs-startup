d6da6ea81c9e015890e2ac6b60966cb8807f520f
Craig Willis <willis8@illinois.edu>
2016-05-18 19:24:52 +0000

    Updated parameters

--- ndslabs-up.sh
+++ ndslabs-up.sh
@@ -4,9 +4,9 @@
 UNAME=$(uname)
 
 if [ "$UNAME" == "Darwin" ]; then
-	echo "Assuming docker-machine"
-	IP_ADDR_MACHINE=$(docker-machine ip)
-	IP_ADDR_PUBLIC=$(docker-machine ip)
+   echo "Assuming docker-machine"
+   IP_ADDR_MACHINE=$(docker-machine ip)
+   IP_ADDR_PUBLIC=$(docker-machine ip)
 elif [ "$UNAME" == "Linux" ]; then
     IP_ADDR_MACHINE=$(ifconfig eth0  | grep "inet " | awk '{print $2}')
     IP_ADDR_PUBLIC=$(curl -s https://api.ipify.org)
@@ -14,36 +14,63 @@ fi
 
 echo -n "Enter the domain name for this server or ENTER to not configure: "
 read domain
-if [ -n "$domain" ]; then 
-	DOMAIN=$domain
-fi
 
-if [ -z $DOMAIN ]; then
+if [ -z "$domain" ]; then
    echo -n "Enter the public IP address for this server [$IP_ADDR_PUBLIC] or ENTER to accept the default: "
    read publicip
    if [ -n "$publicip" ]; then 
-   	IP_ADDR_PUBLIC=$publicip
+      IP_ADDR_PUBLIC=$publicip
    fi
+   APISERVER_HOST="$IP_ADDR_PUBLIC"
+   APISERVER_PORT="30001"
+   APISERVER_SECURE="false"
+   CORS_ORIGIN_ADDR="http://$IP_ADDR_PUBLIC:30000"
+   INGRESS=NodePort
+else
+   DOMAIN=$domain
+   APISERVER_HOST="www.$domain"
+   CORS_ORIGIN_ADDR="https://www.$domain"
+   APISERVER_SECURE="true"
+   APISERVER_PORT="443"
+   INGRESS=LoadBalancer
 fi
+
 
 echo -n "Enter the internal IP address for this server [$IP_ADDR_MACHINE] or ENTER to accept the default: "
-read internaip
-if [ -n "$internaip" ]; then 
-	IP_ADDR_MACHINE=$internaip
+read internalip
+if [ -n "$internalip" ]; then 
+    IP_ADDR_MACHINE=$internalip
 fi
 
-export IP_ADDR_MACHINE
-export IP_ADDR_PUBLIC
-export DOMAIN
 
-kubectl create secret generic ndslabs-tls-secret --from-file=tls.crt=certs/ndslabs.cert --from-file=tls.key=certs/ndslabs.key --namespace=default
-kubectl create -f ndslabs/loadbalancer.yaml
-kubectl create -f ndslabs/default-backend.yaml
+echo "APISERVER_HOST=$APISERVER_HOST"
+echo "APISERVER_PORT=$APISERVER_PORT"
+echo "APISERVER_SECURE=$APISERVER_SECURE"
+echo "CORS_ORIGIN_ADDR=$CORS_ORIGIN_ADDR"
+echo "INGRESS=$INGRESS"
+echo "DOMAIN=$DOMAIN"
+export APISERVER_HOST
+export APISERVER_PORT
+export APISERVER_SECURE
+export CORS_ORIGIN_ADDR
+export INGRESS
+export DOMAIN
+export IP_ADDR_PUBLIC
+export IP_ADDR_MACHINE
+
+
+if [ -n "$DOMAIN" ]; then 
+    kubectl create secret generic ndslabs-tls-secret --from-file=tls.crt=certs/ndslabs.cert --from-file=tls.key=certs/ndslabs.key --namespace=default
+    kubectl create -f ndslabs/loadbalancer.yaml
+    kubectl create -f ndslabs/default-backend.yaml
+    cat ndslabs/default-ingress.yaml | ./mustache | kubectl create -f-
+fi
+
+
 cat ndslabs/gui.yaml | ./mustache | kubectl create -f-
 cat ndslabs/apiserver.yaml | ./mustache | kubectl create -f-
-cat ndslabs/default-ingress.yaml | ./mustache | kubectl create -f-
 
-if [ -n $DOMAIN ]; then 
+if [ -n "$DOMAIN" ]; then 
     echo "After the services start, you should be able to access the NDSLabs UI via:"
     echo "https://www.$DOMAIN"
 else
