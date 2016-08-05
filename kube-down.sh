d7841dd159c456a04b094da3fd908df38ffefbcc
Mike Lambert <lambert8@illinois.edu>
2016-08-05 20:28:18 +0000

    Sync

--- kube-down.sh
+++ kube-down.sh
@@ -4,5 +4,5 @@
 #kubectl drain 127.0.0.1
 #kubectl delete 127.0.0.1
 
-docker stop `docker ps | grep gcr | awk  '{print $1}'`
-docker rm `docker ps -a | grep gcr | awk  '{print $1}'`
+docker stop `docker ps | grep k8s | awk  '{print $1}'`
+docker rm `docker ps -a | grep k8s | awk  '{print $1}'`
