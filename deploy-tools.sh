cebdcbdf37125b9347977ce7ff939868d61bc71c
CoreOS Admin <core@willis8-dev-alpha.os.ncsa.edu>
2016-09-07 22:01:11 +0000

    Added support email and service timeout

--- deploy-tools.sh
+++ deploy-tools.sh
@@ -1,1 +1,1 @@
-docker run -it --name deploy-tools-455 -v `pwd`/deploy-tools:/root/SAVED_AND_SENSITIVE davidraila/deploy-tools:1.0.1-alpha.NDS-445 bash
+docker run -it --name deploy-tools-395 -v `pwd`/../deploy-tools:/root/SAVED_AND_SENSITIVE_VOLUME davidraila/deploy-tools:1.0.1-alpha.NDS-395 bash
