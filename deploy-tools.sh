0c111155e54524044f888689d1cbacec0e5ac3a1
CoreOS Admin <core@willis8-dev-alpha-2.os.ncsa.edu>
2016-10-06 18:49:28 +0000

    Generic deploy-tools starter

--- deploy-tools.sh
+++ deploy-tools.sh
@@ -1,1 +1,1 @@
-docker run -it --name deploy-tools-395 -v `pwd`/../deploy-tools:/root/SAVED_AND_SENSITIVE_VOLUME davidraila/deploy-tools:1.0.1-alpha.NDS-395 bash
+docker run -it --name deploy-tools -v `pwd`/deploy-tools:/root/SAVED_AND_SENSITIVE_VOLUME ndslabs/deploy-tools:latest bash
