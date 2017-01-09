7b7a5f4c05907a8201905b6a695d03b77e5220e4
Mike Lambert <lambert8@illinois.edu>
2017-01-09 20:46:22 +0000

    Trying to read more into this using a cleaner diff

--- kube-down.sh
+++ kube-down.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Remove kubelet first, or else it will continue to respawn killed containers
-docker stop kubelet
+#docker stop kubelet
 docker rm -f kubelet
 
 # Use at your own risk: stop and remove all Docker containers
-docker stop `docker ps | grep gcr | awk  '{print $1}'`
-docker rm `docker ps -a | grep gcr | awk  '{print $1}'`
+#docker stop `docker ps | grep gcr | awk  '{print $1}'`
+docker rm -f `docker ps -a | grep gcr | awk  '{print $1}'`
