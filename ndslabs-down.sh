380ad49f0e719d923ef8b32af9f48cfb74c199ce
Mike Lambert <lambert8@illinois.edu>
2017-01-11 21:35:27 +0000

    Added support for launching ndslabs-logserve

--- ndslabs-down.sh
+++ ndslabs-down.sh
@@ -5,7 +5,8 @@
 #
 
 kubectl delete svc,rc ndslabs-apiserver
-kubectl delete svc,rc ndslabs-gui
+kubectl delete svc,rc ndslabs-webui
+kubectl delete svc ndslabs-logserve
 kubectl delete svc,rc default-http-backend
 kubectl delete rc nginx-ilb-rc 
 kubectl delete ingress ndslabs-ingress
