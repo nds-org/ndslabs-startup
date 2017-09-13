84c4c5a9bed35c23b9c248ccb2322a9e15bee453
Charles <c1mckay@sdsc.edu>
2017-09-12 22:15:29 -0700

    added create/delete user functions

--- account.py
+++ account.py
@@ -1,5 +1,9 @@
 import pexpect
 
+def runShellCmd(shell_cmd):
+	child = pexpect.spawn('/bin/bash', ['-c', shell_cmd])
+	child.expect(pexpect.EOF)
+
 def login():
 	password = pexpect.run('kubectl exec -it ndslabs-apiserver-6rc4k cat password.txt')
 	password = "".join(password.split())
@@ -11,9 +15,27 @@ def login():
 	
 	pexpect.run('ndslabsctl --server https://www.cmdev.ndslabs.org/api list accounts')
 	
+def createUser(name, user_id, email):
+	userFileCreateCommand = 'cat etk.tmpl | sed "s/NAME/{0}/g" | sed "s/USER_ID/{1}/g" | sed "s/EMAIL/{2}/g" > temp.json'.format(name, user_id, email)
+	print userFileCreateCommand
+	userImportCommand = 'ndslabsctl --server https://www.cmdev.ndslabs.org/api import -f temp.json'
+	print userImportCommand
+	runShellCmd(userFileCreateCommand)
+	runShellCmd(userImportCommand)
+	print pexpect.run('cat temp.json')
+	pexpect.run('rm temp.json')
+
+def deleteUser(userName):
+	deleteCmd = 'ndslabsctl --server https://www.cmdev.ndslabs.org/api delete account {0}'.format(userName)
+	pexpect.run(deleteCmd)
+
+def generateUser(pattern):
+
 
 if __name__ == "__main__":
 	login()
+
+	generateUser('user')
 
 	print pexpect.run('ndslabsctl --server https://www.cmdev.ndslabs.org/api list accounts')
 
