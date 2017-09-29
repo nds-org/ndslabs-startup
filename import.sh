1bc20791168822bb1ac0c1ca1aefcbfb8122f2ce
Charles <c1mckay@sdsc.edu>
2017-09-28 18:17:03 -0700

    deleted old files

--- import.sh
+++
@@ -1,4 +0,0 @@
-for id in {1..50}
-do
-   ndslabsctl --server https://www.cmdev.ndslabs.org/api import -f users/user$id.json
-done
--- users.sh
+++
@@ -1,5 +0,0 @@
-for id in {1..50}
-do
- 
-   cat etk.tmpl | sed "s/NAME/User $id/g" | sed "s/USER_ID/user$id/g" | sed "s/EMAIL/user$id@ndslabs.org/g" > users/user$id.json
-done
