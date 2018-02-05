5633477390cf9d605264b9db99c9bca30538d340
Craig Willis <willis8@illinois.edu>
2018-02-05 14:57:52 +0000

    Added check for existing basic auth annotation and changed domain to match usual approach

--- scripts/MigrateSSO/migrate-sso.sh
+++ scripts/MigrateSSO/migrate-sso.sh
@@ -6,8 +6,8 @@
 #
 
 DOMAIN=$1
-signin_url="https://${DOMAIN}/login/#/"
-auth_url="https://${DOMAIN}/cauth/auth"
+signin_url="https://www.${DOMAIN}/login/#/"
+auth_url="https://www.${DOMAIN}/cauth/auth"
 
 set -e
 
@@ -23,9 +23,9 @@ for namespace in $USERS; do
    $KUBECTL_BIN delete secret basic-auth --namespace=${namespace} || true
 
    $ECHO "Migrating ingress for $namespace\n"
-   INGRESS=$($KUBECTL_BIN get ingress --all-namespaces --no-headers | grep -v default | grep -v kube-system | awk '{print $2}')
+   INGRESS=$($KUBECTL_BIN get ingress --namespace=$namespace --no-headers | awk '{print $1}')
    for ingress in $INGRESS; do
        $ECHO "Migrating ingress $INGRESS\n"
-       $KUBECTL_BIN get ingress $ingress --namespace=$namespace --no-headers -o json |  jq ".metadata.annotations={}" | jq ".metadata.annotations={\"nginx.ingress.kubernetes.io/auth-signin\":\"$signin_url\", \"nginx.ingress.kubernetes.io/auth-url\": \"$auth_url\"}" | $KUBECTL_BIN replace --namespace=${namespace} -f -
+       $KUBECTL_BIN get ingress $ingress --namespace=$namespace --no-headers -o json |  jq 'select(.metadata.annotations."ingress.kubernetes.io/auth-type" == "basic")' | jq ".metadata.annotations={}" | jq ".metadata.annotations={\"nginx.ingress.kubernetes.io/auth-signin\":\"$signin_url\", \"nginx.ingress.kubernetes.io/auth-url\": \"$auth_url\"}" | $KUBECTL_BIN replace --namespace=${namespace} -f -
    done  
 done
